2d28f5e60b5e155c72fbee7f041126a1eeb26b78
Noah <noah.yonack@gmail.com>
2016-04-26 18:33:40 -0400

    First commit

--- create_dictionary.py
+++ create_dictionary.py
@@ -16,16 +16,84 @@ txt file.
 
 '''
 import cPickle
+import csv
 
-DATA_FILE = "course_data.txt"
+# the name of the file that contains our pickled dictionary
+COURSE_DATA_DICT_FILENAME = "pickled_course_data.txt"
 
-data = {}
-data[1] = 2
-data[4] = 5
+# the name of the file from which to create the dictionary
+IN_FILENAME = \
+	"../data/2014-10-09 Course Enrollment Data for PTP Project_Jim Waldo.csv"
 
-with open(DATA_FILE, "w") as f:
-	cPickle.dump(data, f)
+# our master dictionary, to be pickled
+course_data_dict = {}
 
-with open(DATA_FILE, "r") as f:
-	lol = cPickle.load(f)
-	print lol
+def parse_date(dateToParse):
+	'''
+	Year and term data is concatenated in IN_FILENAME. For example, a course
+	that takes place during term 0 of year 2004 has "20040" in CSV pointed to 
+	by IN_FILENAME. 
+	
+	This function parses these concatenated dates into two pieces 
+	(e.g., "20040" into 2004 and 0) and returns both pieces of data.
+
+	dataToParse: a string concatenation of a year (e.g., 2004) and a term (0, 1) 
+	returns: a separated year and term (e.g., 2004 and 0) as strings
+	'''
+	year = dateToParse[:-1]
+	term = dateToParse[-1:]
+	return year, term
+
+def pickle_and_save():
+	'''
+	Pickles our master course dictionary and saves it to 
+	COURSE_DATA_DICT_FILENAME
+	'''
+	with open(COURSE_DATA_DICT_FILENAME, "w") as f:
+		cPickle.dump(course_data_dict, f)
+
+def unpickle_and_print():
+	'''
+	Deserializes our pickled course dictionary and prints it to the console
+	'''
+	with open(COURSE_DATA_DICT_FILENAME, "r") as f:
+		print cPickle.load(f)
+
+# construct the dictionary
+with open(IN_FILENAME) as f:
+	# create a CSV reader object
+	reader = csv.reader(f, delimiter='\t')
+	
+	# skip the first line so we don't read in the CSV header
+	reader.next()
+	
+	for row in reader:	
+		# row[1] represents the concatanted date that needs parsing (e.g. "20041")
+		year, term = parse_date(row[1])
+		course_id = row[2]
+
+		# construct the dictionary by checking for keys and incrementing
+		# when necessary. We only need to increment when the full dictionary
+		# path to a course already exists (e.g., when the data structure already
+		# has keys for the course_id, the year, and the term)
+		
+		# check if course_id already exists in the dictionary
+		if course_id in course_data_dict:
+			# check if the year for a given course_id exists
+			if year in course_data_dict[course_id]:
+				# check if a term for a given year for a given course exits
+				if term in course_data_dict[course_id][year]:
+					# if so, increment the enrollment by 1
+					course_data_dict[course_id][year][term] += 1 
+				else:
+					course_data_dict[course_id][year][term] = 1
+			else:
+				course_data_dict[course_id][year] = {}
+				course_data_dict[course_id][year][term] = 1
+		else:
+			course_data_dict[course_id] = {}
+			course_data_dict[course_id][year] = {}
+			course_data_dict[course_id][year][term] = 1
+
+pickle_and_save()
+unpickle_and_print()
