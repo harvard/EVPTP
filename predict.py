e2cc305df5ff638784e581ce37911306ee697911
Noah <noah.yonack@gmail.com>
2016-05-02 16:38:36 -0400

    Commented prediction code

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 '''
 This file predicts course enrollments using a linear regression
-and writes these predictions to a file
+and writes these predictions to a file.
 '''
 
 import csv
@@ -14,6 +14,10 @@ COURSE_DATA_DICT_FILENAME = "pickled_course_data.txt"
 # the name of the file to which we write predictions
 COURSE_PREDICTIONS_FILENAME = "course_predictions.csv"
 
+# our pickled dictionary has data up until 2014, so we want to predict
+# enrollments for 2015
+YEAR_TO_PREDICT = 2015
+
 # load our course enrollment data by file
 with open(COURSE_DATA_DICT_FILENAME, "r") as f:
 		 course_data_dict = cPickle.load(f)
@@ -23,10 +27,11 @@ with open(COURSE_PREDICTIONS_FILENAME, "w") as predictions_file:
 	# prepare CSV file for writing
 	writer = csv.DictWriter(predictions_file, 
 		fieldnames=["course_id", "year", "fall_prediction", "spring_prediction"])
+	# write the fieldnames to the header of the CSV
 	writer.writeheader()
 
-	# construct two enrollment lists (one for each term) for each course
 	for course_id in course_data_dict:
+		# construct two enrollment lists (one for each term) for each course
 		enrollment_data_dict = {
 			"FALL": [],
 			"SPRING": []
@@ -37,15 +42,13 @@ with open(COURSE_PREDICTIONS_FILENAME, "w") as predictions_file:
 		for academic_year in course_data_dict[course_id]:
 			for term in ["FALL", "SPRING"]:
 				if term in course_data_dict[course_id][academic_year]:
+					# grab the enrollment from the unpickled dictionary
 					enrollment = course_data_dict[course_id][academic_year][term]
 					# push in a tuple of academic year and enrollment for that year.
 					# ex. (2014, 100)
 					enrollment_data_dict[term].append((academic_year, enrollment))
 
-		# now that we have a history enrollments for both FALL and SPRING 
-		# semesters, we can begin predicting
-		YEAR_TO_PREDICT = 2015
-
+		# a dictionary to hold our predictions for the given course
 		predicted_enrollments = {
 			"FALL": -1,
 			"SPRING": -1
@@ -53,17 +56,25 @@ with open(COURSE_PREDICTIONS_FILENAME, "w") as predictions_file:
 
 		# predict fall enrollments if we have fall data
 		for term in ["FALL", "SPRING"]:
+			# if we actually have data to predict on...
 			if len(enrollment_data_dict[term]) > 0:
 				regr = linear_model.LinearRegression()
 				
+				# grab all the years
 				x_train = np.array([int(pairing[0]) for pairing in enrollment_data_dict[term]]).reshape(-1, 1)
+				# grab all the enrollments
 				y_train = np.array([int(pairing[1]) for pairing in enrollment_data_dict[term]]).reshape(-1, 1)
+
+				# fit the data
 				regr.fit(x_train, y_train)
 
+				# now predict the YEAR_TO_PREDICT
 				predictions = regr.predict(np.array([YEAR_TO_PREDICT]).reshape(1, -1))
-				predicted_enrollments[term] = round(predictions[0][0], 3)
 
-		# write predictions to a file
+				# add the predictions to the predicted_enrollments dictionary
+				predicted_enrollments[term] = round(predictions[0][0], 3) # round to 3 decimal places
+
+		# to be written to a file
 		prediction_csv_row = {
 			"course_id": course_id,
 			"year": YEAR_TO_PREDICT,
