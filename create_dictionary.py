a8b5d9c13be6aa9831eae53a26abdd900f838a58
Jim Waldo <jiniwaldo@gmail.com>
2016-06-17 11:44:14 -0400

    Re-factored create_dictionary.py to allow passing in the csv file from which to read the course data, and to append to a dictionary if one has already been created (by reading in the created file, and then adding the contents of the csv file to the dictionary). Note that if you append a file that has already been read, it will add the enrollment again; this should be fixed but right now we should make sure never to run the same file through twice.
    Added code to make create_dictionary.py idempotent if run on the same csv file with a dictionary that has already added
    the contents of that csv. When an existing dictionary is read, a set is created that will keep track of the course-id,
    years, and term for the courses in the dictionary. If this combination is found in the csv being read in, it will be
    skipped.
    
    Wrote create_dictionary_new.py, which will convert the new form of .csv received from the SIS group to a dictionary that
    is in the same form as that produced by create_dictionary.py. If I wanted to be clever, I could merge the code into
    create dictionary (and add a flag or a configuration to determine which of the formats the original csv is in), but
    since the format of the dumps are likely to change, I dicided just to write a new program. It does, however, re-use
    much of the original program.
    
    In doing this, I found a somewhat simpler way of determining if the dictionary entries have been created, and so went
    back and changed create_dictionary.py to use the simpler algorithm.

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,5 +1,5 @@
 '''
-This program consumes some of the data files used in past PTP prediction
+This program consumes the data files used in PTP prediction
 attempts to build a multi-level dictionary that has the following form:
 
 {
@@ -10,23 +10,32 @@ attempts to build a multi-level dictionary that has the following form:
 	}
 }
 
+The program can also be used to add new information to the dictionary; if an existing file with a dictionary
+is given as the output file, the dictionary is first read, and a set of the courseId+Year+Term is created and
+records for those existing entries will not be calculated
+
 Imports cPickle, a serialization library for Python objects. This package is
 used to pickle our master course dictionary into a byte_stream saved as a 
-txt file.
+txt file. Imports csv to read the input file, sys to get the command-line arguments, and
+os.path to determine if the pickle file already exists.
 
 '''
 import cPickle
-import csv
+import csv, sys, os.path
 
-# the name of the file that contains our pickled dictionary
-COURSE_DATA_DICT_FILENAME = "converted_pickled_course_data.txt"
-
-# the name of the file from which to create the dictionary
-IN_FILENAME = \
-    "../AllData/converted_course_data.csv"
-# our master dictionary, to be pickled
-course_data_dict = {}
-
+def get_csv(infile_name):
+    '''
+    Take an input string naming an existing file and create a csv reader for the file. Part of
+    this routine will determine the delimiter used in the file, as we have been getting both
+    comma- and tab-separated files for our data.
+    :param infile_name: Name of an existing file containing a csv-formatted course enrollment data
+    :return: A csv-reader that is set to the appropriate delimiter.
+    '''
+    inf = open(infile_name, 'r')
+    dialect = csv.Sniffer().sniff(inf.read(1024))
+    inf.seek(0)
+    csv_r = csv.reader(inf, delimiter=dialect.delimiter)
+    return csv_r
 
 def parse_date(dateToParse):
     '''
@@ -51,58 +60,108 @@ def parse_date(dateToParse):
     return year, term
 
 
-def pickle_and_save():
+def pickle_and_save(course_data_dict, outf):
     '''
-    Pickles our master course dictionary and saves it to
-    COURSE_DATA_DICT_FILENAME
+    Pickles our master course dictionary and saves it to the file named by outf. If this file
+    already exists, it will be over-written by the newly constructed dictionary.
     '''
-    with open(COURSE_DATA_DICT_FILENAME, "w") as f:
-        cPickle.dump(course_data_dict, f)
+    wfile = open(outf, 'w')
+    cPickle.dump(course_data_dict, wfile)
+    wfile.close()
 
 
-def unpickle_and_print():
+def unpickle_and_print(from_file):
     '''
     Deserializes our pickled course dictionary and prints it to the console
     '''
-    with open(COURSE_DATA_DICT_FILENAME, "r") as f:
+    with open(from_file, "r") as f:
         print cPickle.load(f)
 
+def unpickle_and_build_set(pickle_file):
+    '''
+    Opens the file named by the parameter and reconstructs the dictionary that was pickled in that file. Using that
+    dictionary, builds a set of concatenated courseId/Year/term that are in a dictionary, so that those courses will not
+    have thier enrollment counted (actually, added to) by the main() function.
+    :param pickle_file: name of the file containing the pickle of the dictionary that already exists and is being
+    added to
+    :return: A pair of a dictionary as specified at the beginning of the file, and a set whose entries are the
+    concatenation of courseId/Year/Term that already appear in the dictionary
+    '''
+    r = open(pickle_file, 'r')
+    course_data_dict = cPickle.load(r)
+    course_set = set()
+    r.close()
+    for k, v in course_data_dict.iteritems():
+        for y, v1 in v.iteritems():
+            for t, v2 in v1.iteritems():
+                course_set.add(''.join((k, y, t)))
 
-# construct the dictionary
-with open(IN_FILENAME) as f:
-    # create a CSV reader object
-    reader = csv.reader(f, delimiter='\t')
+    return course_data_dict, course_set
 
+
+def main(reader, course_data_dict, course_set, out_file):
+    '''
+    Takes an open csv.reader, a dictionary, a set containing course/year/term already in the dictionary,
+    and the name of a file to write the pickle of the full
+    dictionary, and extracts the dictionary of course information needed from the csv file, writing
+    it to the dictionary. The dictionary may be empty or may have previous entries in it; if there are
+    previous entries this is assumed to be an addition to the dictionary
+    :param reader: an open csv reader containing the class enrollment information
+    :param course_data_dict: a dictionary, as specified in the header of this program. If empty, we are
+    creating a whole new dictionary; if it is not empty we are appending the an existing data set
+    :param course_set: a set containing the concatenated course/year/term groupings that are already in the dictionary
+    :param out_file: The name of the output file; this is where the pickle of the final dictionary will
+    be placed
+    :return: None
+    '''
+    # construct the dictionary
     # skip the first line so we don't read in the CSV header
     reader.next()
-
     for row in reader:
         # row[1] represents the concatanted date that needs parsing (e.g. "20041")
         year, term = parse_date(row[1])
         course_id = row[2]
+        #first, check to see if this is in the dictionary already; if not add it
+        if ''.join((course_id, year, term)) not in course_set:
+            # construct the dictionary by checking for keys and incrementing
+            # when necessary. We only need to increment when the full dictionary
+            # path to a course already exists (e.g., when the data structure already
+            # has keys for the course_id, the year, and the term)
 
-        # construct the dictionary by checking for keys and incrementing
-        # when necessary. We only need to increment when the full dictionary
-        # path to a course already exists (e.g., when the data structure already
-        # has keys for the course_id, the year, and the term)
-
-        # check if course_id already exists in the dictionary
-        if course_id in course_data_dict:
-            # check if the year for a given course_id exists
-            if year in course_data_dict[course_id]:
-                # check if a term for a given year for a given course exits
-                if term in course_data_dict[course_id][year]:
-                    # if so, increment the enrollment by 1
-                    course_data_dict[course_id][year][term] += 1
-                else:
-                    course_data_dict[course_id][year][term] = 1
-            else:
+            # check if course_id does not exists in the dictionary, if so create an entry and a dictionary
+            # as its value
+            if course_id not in course_data_dict:
+                course_data_dict[course_id] = {}
+            # check if the year is in the dictionary for the course id; if not create an entry with a
+            # dictionary as its value.
+            if year not in course_data_dict[course_id]:
                 course_data_dict[course_id][year] = {}
+            # check if the term is in the dictionary for the course and year; if not create an entry and
+            # initialize the count to one
+            if term not in course_data_dict[course_id][year]:
                 course_data_dict[course_id][year][term] = 1
-        else:
-            course_data_dict[course_id] = {}
-            course_data_dict[course_id][year] = {}
-            course_data_dict[course_id][year][term] = 1
+            else:
+                # if the course/year/term is in the dictionary, increment the count for that entry
+                course_data_dict[course_id][year][term] += 1
 
-pickle_and_save()
-unpickle_and_print()
+    pickle_and_save(course_data_dict, out_file)
+    #unpickle_and_print(out_file)
+
+
+if __name__ == '__main__':
+    in_data = sys.argv[1]
+    if not os.path.exists(in_data):
+        print 'Specified input file does not exist'
+        print 'Usage: python create_dictionary.py class_file.csv output_dictionary'
+        exit(1)
+
+    csv_in = get_csv(in_data)
+
+    out_file = sys.argv[2]
+    if os.path.exists(out_file):
+        course_data_dict, course_set =unpickle_and_build_set(out_file)
+    else:
+        course_data_dict = {}
+        course_set = set()
+
+    main(csv_in, course_data_dict, course_set, out_file)
--- predict.py
+++ predict.py
@@ -22,67 +22,66 @@ YEAR_TO_PREDICT = 2015
 
 # load our course enrollment data by file
 with open(COURSE_DATA_DICT_FILENAME, "r") as f:
-		 course_data_dict = cPickle.load(f)
+    course_data_dict = cPickle.load(f)
 
 with open(COURSE_PREDICTIONS_FILENAME, "w") as predictions_file:
-	
-	# prepare CSV file for writing
-	writer = csv.DictWriter(predictions_file, 
-		fieldnames=["course_id", "year", "fall_prediction", "spring_prediction"])
-	# write the fieldnames to the header of the CSV
-	writer.writeheader()
+    # prepare CSV file for writing
+    writer = csv.DictWriter(predictions_file,
+                            fieldnames=["course_id", "year", "fall_prediction", "spring_prediction"])
+    # write the fieldnames to the header of the CSV
+    writer.writeheader()
 
-	for course_id in course_data_dict:
-		# construct two enrollment lists (one for each term) for each course
-		enrollment_data_dict = {
-			"FALL": [],
-			"SPRING": []
-		}
+    for course_id in course_data_dict:
+        # construct two enrollment lists (one for each term) for each course
+        enrollment_data_dict = {
+            "FALL": [],
+            "SPRING": []
+        }
 
-		# for each academic year, we will have 2 pieces of data: fall enrollment
-		# and spring enrollment
-		for academic_year in course_data_dict[course_id]:
-			for term in ["FALL", "SPRING"]:
-				if term in course_data_dict[course_id][academic_year]:
-					# grab the enrollment from the unpickled dictionary
-					enrollment = course_data_dict[course_id][academic_year][term]
-					# push in a tuple of academic year and enrollment for that year.
-					# ex. (2014, 100)
-					enrollment_data_dict[term].append((academic_year, enrollment))
+        # for each academic year, we will have 2 pieces of data: fall enrollment
+        # and spring enrollment
+        for academic_year in course_data_dict[course_id]:
+            for term in ["FALL", "SPRING"]:
+                if term in course_data_dict[course_id][academic_year]:
+                    # grab the enrollment from the unpickled dictionary
+                    enrollment = course_data_dict[course_id][academic_year][term]
+                    # push in a tuple of academic year and enrollment for that year.
+                    # ex. (2014, 100)
+                    enrollment_data_dict[term].append((academic_year, enrollment))
 
-		# a dictionary to hold our predictions for the given course
-		predicted_enrollments = {
-			"FALL": -1,
-			"SPRING": -1
-		}
+        # a dictionary to hold our predictions for the given course
+        predicted_enrollments = {
+            "FALL": -1,
+            "SPRING": -1
+        }
 
-		# predict fall enrollments if we have fall data
-		for term in ["FALL", "SPRING"]:
-			# if we actually have data to predict on...
-			if len(enrollment_data_dict[term]) > 0:
-				regr = linear_model.LinearRegression()
-				
-				# grab all the years
-				x_train = np.array([int(pairing[0]) for pairing in enrollment_data_dict[term]]).reshape(-1, 1)
-				# grab all the enrollments
-				y_train = np.array([int(pairing[1]) for pairing in enrollment_data_dict[term]]).reshape(-1, 1)
+        # predict fall enrollments if we have fall data
+        for term in ["FALL", "SPRING"]:
+            # if we actually have data to predict on...
+            if len(enrollment_data_dict[term]) > 0:
+                regr = linear_model.LinearRegression()
 
-				# fit the data
-				regr.fit(x_train, y_train)
+                # grab all the years
+                x_train = np.array([int(pairing[0]) for pairing in enrollment_data_dict[term]]).reshape(-1, 1)
+                # grab all the enrollments
+                y_train = np.array([int(pairing[1]) for pairing in enrollment_data_dict[term]]).reshape(-1, 1)
 
-				# now predict the YEAR_TO_PREDICT
-				predictions = regr.predict(np.array([YEAR_TO_PREDICT]).reshape(1, -1))
+                # fit the data
+                regr.fit(x_train, y_train)
 
-				# add the predictions to the predicted_enrollments dictionary
-				predicted_enrollments[term] = round(predictions[0][0], 3) # round to 3 decimal places
+                # now predict the YEAR_TO_PREDICT
+                predictions = regr.predict(np.array([YEAR_TO_PREDICT]).reshape(1, -1))
 
-		# to be written to a file
-		prediction_csv_row = {
-			"course_id": course_id,
-			"year": YEAR_TO_PREDICT,
-			"fall_prediction": predicted_enrollments["FALL"],
-			"spring_prediction": predicted_enrollments["SPRING"]
-		}
+                # add the predictions to the predicted_enrollments dictionary
+                predicted_enrollments[term] = round(predictions[0][0], 3)  # round to 3 decimal places
 
-		writer.writerow(prediction_csv_row)
-		print prediction_csv_row
+        # to be written to a file
+        prediction_csv_row = {
+            "course_id": course_id,
+            "year": YEAR_TO_PREDICT,
+            "fall_prediction": predicted_enrollments["FALL"],
+            "spring_prediction": predicted_enrollments["SPRING"]
+        }
+
+        writer.writerow(prediction_csv_row)
+        print prediction_csv_row
